3fdc985258a9b447403c0cfcc4474d63a3537b37
wqquan.wang <wqquan.wang@qunar.com>
2018-12-29 11:56:06 +0800

    Initial commit

---
+++ src/test/java/Demo01.java
@@ -0,0 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by wqquan.wang on 2018/12/26.
+ */
+public class Demo01 {
+    @Test
+    public void test01() {
+        Assert.assertTrue(false);
+    }
+}
